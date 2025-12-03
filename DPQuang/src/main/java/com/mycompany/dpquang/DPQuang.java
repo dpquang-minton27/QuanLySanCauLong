8d3afa998ce236b3b751c4e9d231f50f98001fc5
Dương Phước Quang <duongphuocquang133@gmail.com>
2025-12-03 19:50:19 +0700

    Delete DPQuang directory

--- DPQuang/src/main/java/com/mycompany/dpquang/DPQuang.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.dpquang;
-
-public class DPQuang {
-
-    public static void main(String[] args) {
-        // Gọi lại main trong class Main
-        Main.main(args);
-    }
-}
--- DPQuang/src/main/java/com/mycompany/dpquang/IQuanLySan.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.dpquang;
-
-public interface IQuanLySan {
-    void themSan(SanCauLong san);
-    void xoaSan(String maSan);
-    void capNhatSan(String maSan, SanCauLong san);
-    SanCauLong timSan(String maSan);
-    void hienThiTatCa();
-}
